926abe51da376c7fa26379049f618e8a4d5055f9
N1belungen <ZengDuo0927@163.com>
2021-09-02 22:22:56 +0800

    springboot启动器

---
+++ src/main/java/com/n1belungen/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.n1belungen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringbootDemoApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
